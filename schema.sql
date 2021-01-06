179c7129acc2e88c2c20e1376d0b89561bdf056d
dannygarfield <dgarfield25@gmail.com>
2021-01-06 14:39:55 -0500

    remove unused tables from schema

--- schema.sql
+++ schema.sql
@@ -4,14 +4,3 @@ CREATE TABLE IF NOT EXISTS entries
 , happened_at TEXT
 , amount TEXT
 );
-
-CREATE TABLE IF NOT EXISTS buckets
-(id INTEGER PRIMARY KEY
-, name TEXT
-, asset INT
-, liquidity TEXT
-);
-
-CREATE TABLE IF NOT EXISTS dateseries
-(day TEXT PRIMARY KEY
-);
