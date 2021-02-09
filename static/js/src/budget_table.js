b5564f2cfb44cb0ac26e5aa6a969aafb77311f8d
dannygarfield <dgarfield25@gmail.com>
2021-02-09 12:24:49 -0500

    add jsx to project

---
+++ static/js/src/budget_table.js
@@ -0,0 +1,236 @@
+'use strict';
+
+// to start jsx pre-processor:
+// npx babel --watch static/js/src --out-dir static/js --presets react-app/prod
+
+class EntryRow extends React.Component {
+  render() {
+    const entry = this.props.entry
+    return (
+      <tr>
+        <td>{entry.EntryDate}</td>
+        <td>${entry.Amount}</td>
+        <td>{entry.Category}</td>
+        <td>{entry.Description}</td>
+      </tr>
+    );
+  }
+}
+
+class TableRows extends React.Component {
+  render() {
+    const rows = [];
+    this.props.entries.forEach((entry, index) => {
+      entry.EntryDate = formatDate(entry.EntryDate);
+      entry.Amount = entry.Amount / 100;
+      rows.push(
+        <EntryRow
+          entry={entry}
+          key={index}/>
+      );
+    });
+
+    return (
+        <tbody>
+          {rows}
+        </tbody>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+    );
+  }
+}
+
+class DateFilters extends React.Component {
+  constructor(props) {
+    super(props);
+    const entries = this.props.entries;
+    const start = formatDate(entries[0].EntryDate);
+    const end = formatDate(entries[entries.length - 1].EntryDate);
+
+    this.state = {
+      startDate: start,
+      endDate: end
+    };
+  }
+
+  handleFilterChange = this.handleFilterChange.bind(this);
+
+  handleFilterChange(e) {
+    const value = e.target.value
+    const name = e.target.name
+    this.setState(state => ({
+        [name]: value
+    }));
+  }
+
+  render() {
+    return (
+      <form>
+        <label className="filters">start:</label>
+        <input
+          type="date"
+          name="startDate"
+          value={this.state.startDate}
+          onChange={this.handleFilterChange} />
+        <br />
+        <label className="filters">end:</label>
+        <input
+          type="date"
+          name="endDate"
+          value={this.state.endDate}
+          onChange={this.handleFilterChange} />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class BudgetTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>budget table</h2>
+        <DateFilters entries={this.props.entries}/>
+        <br />
+        <table>
+          <Header />
+          <TableRows
+            entries={this.props.entries}
+          />
+        </table>
+      </div>
+    );
+  }
+}
+
+class EntryForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      happened_at: '',
+      amount: '',
+      category: '',
+      description: ''
+    };
+  }
+
+  handleInputChange = this.handleInputChange.bind(this)
+
+  handleInputChange(e) {
+    const name = e.target.name
+    const value = e.target.value
+    this.setState((state) => ({
+      [name]: value
+    }));
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Insert a budget entry</h2>
+        <form>
+          <label className="entry-form">happened_at</label>
+          <input
+            name="happened_at"
+            type="text"
+            value={this.state.happened_at}
+            onChange={this.handleInputChange} />
+          <br />
+          <label className="entry-form">amount</label>
+          <input
+            name="amount"
+            type="text"
+            value={this.state.amount}
+            onChange={this.handleInputChange}/>
+          <br />
+          <label className="entry-form">category</label>
+          <input
+            name="category"
+            type="text"
+            value={this.state.category}
+            onChange={this.handleInputChange} />
+          <br />
+          <label className="entry-form">amount</label>
+          <input name="description" type="text" />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+class BudgetPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      jsonEntries: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("fetching budget.json ...");
+    fetch('http://localhost:8080/budget.json')
+      .then(response => response.json())
+      .then(responseData => {
+        console.log("success!");
+        console.log(responseData);
+        this.setState({ jsonEntries: responseData });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
+  render() {
+    if (this.state.jsonEntries.length > 0) {
+        return (
+          <div>
+            <h1>welcome!</h1>
+            <EntryForm />
+            <BudgetTable entries={this.state.jsonEntries}/>
+          </div>
+        );
+    } else {
+        return (<p>waiting for entries to load...</p>);
+    }
+  }
+}
+
+const ENTRIES = [
+  {EntryDate: "2021-02-01", Amount: 504.24, Category: "health", Description: "COBRA"},
+  {EntryDate: "2021-02-01", Amount: 1500.00, Category: "rent", Description: "-"},
+  {EntryDate: "2021-02-02", Amount: 180.85, Category: "groceries", Description: "DeCicco"},
+  {EntryDate: "2021-02-03", Amount: 150.00, Category: "investing", Description: "Public.com"},
+]
+
+function formatDate(inputDate) {
+  let [month, day, year] = new Date(inputDate).toLocaleDateString("en-US").split("/");
+  if (month.length < 2) {
+      month = '0' + month;
+  }
+  if (day.length < 2) {
+      month = '0' + month;
+  }
+  return [year, month, day].join("-");
+}
+
+ReactDOM.render(
+  <BudgetPage constEntries={ENTRIES}/>,
+  document.querySelector('#container')
+);
